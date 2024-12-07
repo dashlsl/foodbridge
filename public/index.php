dbbfada78f85e85dc4a58fe96f25178567fc318d
dashlsl <dashaynelumsl@gmail.com>
2024-12-07 22:24:32 +0800

    Update initial commit
    To create a better framework

--- public/index.php
+++ public/index.php
@@ -1,10 +1,18 @@
 <?php 
 
-session_start();
+/**
+ * FoodBridge - Description
+ * 
+ * @package FoodBridge
+ * @version 
+ * @author
+ * - github.com/dashlsl
+ * - github.com/OwenTay66
+ * 
+ * @dependencies
+ * 
+ */
 
-require "../app/core/init.php";
-
-DEBUG ? ini_set('display_errors', 1) : ini_set('display_errors', 0);
-
-$app = new App;
-$app->loadController();
+ define('ROOT', __DIR__);
+ define('ROOT_URI', $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/foodbridge');
+ define('ACCESS', true);
