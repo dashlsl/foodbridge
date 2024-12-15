a5b32c509e06373e4ccc644f4f5e1a45deeef9b5
dashlsl <dashaynelumsl@gmail.com>
2024-12-15 19:14:01 +0800

    Update views and routes

--- app/routes/routes.php
+++ app/routes/routes.php
@@ -9,7 +9,8 @@ use App\Routes\RouteManager;
 
 $rm = new RouteManager;
 
-// Add GET routes here
+/** GET ROUTES */
+// Index
 $rm->get(
     '/', 
     'IndexController@index');
@@ -17,5 +18,34 @@ $rm->get(
 $rm->get(
     '/index.php', 
     'IndexController@index');
+
+// User
+$rm->get(
+    '/register',
+    'UserController@registerView');
+
+$rm->get(
+    '/login',
+    'UserController@loginView');
+
+$rm->get(
+    '/edit-profile',
+    'UserController@editProfileView');
+
+$rm->get(
+    '/logout',
+    'UserController@logout');
+
+// Admin
+$rm->get(
+    '/admin-user-list',
+    'AdminController@adminUserListView');  // Default view
+
+$rm->get(
+    '/admin-order',
+    'AdminController@adminOrderView');
+
+
+
 
 $rm->handleRoute($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
